ed78af974c7e532a28ac2fe4c2de79dcb1b60c73
Jerry-Mubiru <40108767+Jerry-Mubiru@users.noreply.github.com>
2023-10-09 13:42:30 -0400

    Alert And Deliverable class.
    Deliverable class is used to contain email content.

--- Alert.py
+++ Alert.py
@@ -1,59 +1,37 @@
 #import the abstract class module
 from abc import ABC,abstractmethod
+from deliverable import Deliverable
 
 #class definition of a generic abstract Alert
-class Alert(ABC):
+class Alert(Deliverable, ABC):
 
-    #methods to return the title and message
-    @abstractmethod
+    #methods to return the title and message defined in The Deliverable
     def get_title(self):
-        pass
-    @abstractmethod
+        return self.email_subject
     def get_message(self):
-        pass 
+        return self.email_message 
 
 #Subclasses inheriting from abstract class Alert
 #Alert A_B (Main Power Off)
 class Alert_Main_Power_Off(Alert):
     #instantiate the variables
     def __init__(self):
-        super().__init__()
-        self.title = "[CARC] - MAIN POWER OFF"
-        self.message = "The main power has been turned off and the system is currently running on battery power."
-    def get_title(self):
-        return self.title
-    def get_message(self):
-        return self.message
+        super().__init__("[CARC] - MAIN POWER OFF","The main power has been turned off and the system is currently running on battery power.")
+
 #Alert B_A and C_D (Main Power Restored)
 class Alert_Main_Power_On(Alert):
     #instantiate the variables
     def __init__(self):
-        super().__init__()
-        self.title = "[CARC] - MAIN POWER RESTORED"
-        self.message = "The main power has been restored. The system is currently running on main."
-    def get_title(self):
-        return self.title
-    def get_message(self):
-        return self.message
+        super().__init__("[CARC] - MAIN POWER RESTORED","The main power has been restored. The system is currently running on main.")
+
 #Alert B_C (Critical Battery Depletion)
 class Alert_Battery_Critical(Alert):
     #instantiate the variables
     def __init__(self):
-        super().__init__()
-        self.title = "[CARC] - BATTERY AT CRITICAL LEVEL"
-        self.message = "The battery has depleted below 20%! Restore main power or prepare for shutdown."
-    def get_title(self):
-        return self.title
-    def get_message(self):
-        return self.message
+        super().__init__("[CARC] - BATTERY AT CRITICAL LEVEL","The battery has depleted below 20%! Restore main power or prepare for shutdown.")
+
 #Alert D_A (Battery Sufficiently Charged)
 class Alert_Battery_Charged(Alert):
     #instantiate the variables
     def __init__(self):
-        super().__init__()
-        self.title = "[CARC] - BATTERY SUFFICIENTLY CHARGED"
-        self.message = "The battery has been sufficiently charged."
-    def get_title(self):
-        return self.title
-    def get_message(self):
-        return self.message
+        super().__init__("[CARC] - BATTERY SUFFICIENTLY CHARGED","The battery has been sufficiently charged.")
---
+++ deliverable.py
@@ -0,0 +1,25 @@
+#import abstract class functionality
+from abc import ABC,abstractmethod
+
+#Defines a class Deliverable which is sent by the email class
+#Any class inheriting from Deliverable can be sent as an email
+class Deliverable(ABC):
+
+    #variables for the email
+    TO_ADDRESS = "jcm253@case.edu"
+    FROM_ADDRESS = "mubiru.jerry1@gmail.com"
+    email_subject = ""
+    email_message = ""
+
+    #constructor to initialize the subject and email whenever a Deliverable is created
+    def __init__(self, subject: str, message: str):
+        self.email_subject = subject
+        self.email_message = message
+
+    #abstract methods to get the title and message
+    @abstractmethod
+    def get_title(self):
+        pass
+    @abstractmethod
+    def get_message(self):
+        pass
