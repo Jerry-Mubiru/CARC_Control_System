4cc240d7cf51f8ccbd9cb00682ea0c4b7cb35fba
Jerry-Mubiru <40108767+Jerry-Mubiru@users.noreply.github.com>
2023-10-12 14:30:11 -0400

    Email class
    Describes the class to send the emails when triggered.

---
+++ email.py
@@ -0,0 +1,22 @@
+#import the Deliverable class
+from deliverable import Deliverable
+import smtplib
+
+#class definition of the email sending class
+class Email:
+    #stores the password to the email address
+    password = "<<insert password here>>"
+    #define a function
+    def send_email(self, deliverable: Deliverable):
+        #create an email client
+        smtp_client = smtplib.SMTP('smtp.gmail.com', 587)
+        #call the ehlo function
+        smtp_client.ehlo()
+        #Start a tls connection
+        smtp_client.starttls()
+        #login to the email account
+        smtp_client.login(deliverable.FROM_ADDRESS, self.password)
+        #send the email with the deliverable's information
+        smtp_client.sendmail(deliverable.FROM_ADDRESS, deliverable.TO_ADDRESS, f'Subject:{deliverable.email_subject}\n {deliverable.email_message}\n Sincerely, \n Case Amateur Radio Club.')
+        #close the connection
+        smtp_client.quit()
